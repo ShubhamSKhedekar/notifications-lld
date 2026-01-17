67e7c2b72742a5187a32696300a33abae754d31b
Shubham Khedekar <125666627+ShubhamSKhedekar@users.noreply.github.com>
2026-01-17 08:18:01 +0000

    Commit 1 - Application set up

---
+++ notificationslld/src/main/java/com/notifications/lld/App.java
@@ -0,0 +1,12 @@
+package com.notifications.lld;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        //java -cp target/notificationslld-1.0-SNAPSHOT.jar com.notifications.lld.App
+        System.out.println( "Hello World! Learn Notifications LLD" );
+
+        
+    }
+}
